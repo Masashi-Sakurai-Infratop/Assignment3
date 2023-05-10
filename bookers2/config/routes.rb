5b5ca31cae107075df767c63d70aba88e404dbc4
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-05-10 00:53:19 +0000

    Upadate モデルとコントローラの定義、ページのヘッダーを作成

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  get "/home/about", to: "homes#about", as: "home_about"
+  devise_for :users
+
+  resources :users, only: [:index, :show]
+  resources :books, only: [:index, :show, :edit]
 end
