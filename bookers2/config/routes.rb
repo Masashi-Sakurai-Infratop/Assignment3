2712d772f4ce84a051b1ce92089ea9456d1c1ac2
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-05-11 00:20:14 +0000

    Upadate Footerの追加

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/home/about", to: "homes#about", as: "home_about"
   devise_for :users
-
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :edit]
 end
