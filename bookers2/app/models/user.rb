ea6d2c315a664f2e37e30ae4fd1fcf19d6332562
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-05-13 22:51:49 +0000

    Update nameカラムをuniqueに設定

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
-  validates :name, length: { in: 2..20 }, presence: true
+  validates :name, length: { in: 2..20 }, uniqueness: true, presence: true
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
