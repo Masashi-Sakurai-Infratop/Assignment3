ff7085c95c0dd47b8a56c2e330f053de1fb13053
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-05-13 05:32:22 +0000

    Update Bookの編集ページの追加

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -11,19 +11,31 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @new_book = Book.new(book_params)
     @new_book.user_id = current_user.id
     if @new_book.save
-      flash[:notice] = "You have created book successfully."
+      flash[:notice] = "You have created the book successfully."
       redirect_to book_path(@new_book.id)
     else
       @books = Book.all
       @user = User.find(current_user.id)
       render :index
     end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated the book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+
   end
 
   def destroy
