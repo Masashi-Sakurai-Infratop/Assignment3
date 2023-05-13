0e7534b17774c9219166f6592ab8ac0d776fabd7
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-05-13 06:54:32 +0000

    Update Success and Error フラッシュメッセージ機能の完成

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
+  validates :name, length: { in: 2..20 }, presence: true
+  validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
 
